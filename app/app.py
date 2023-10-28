5da1296de666d536b62cd423e23a962c1986059e
frandergomez <98371312+frandergomez@users.noreply.github.com>
2023-10-28 12:59:34 -0600

    agregar mensaje

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Datos donde almacenar los mensajes procesados en formato XML
+messages_data = []
+
+@app.route('/grabarMensaje', methods=['POST'])
+def grabar_mensaje():
+    if 'file' not in request.files:
+        return jsonify({
+            "message": "No se ha enviado un archivo, intente de nuevo"
+        })
+    file = request.files['file']
+    file_contents = file.read().decode('utf-8')
+    messages_data.append({"message": file_contents, "hashtag": "example_hashtag"})
+    return jsonify({
+        "message": "El mensaje fue grabado con éxito"
+    })
